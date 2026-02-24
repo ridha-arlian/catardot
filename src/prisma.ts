9fc18c32c36b2622f2b15e00f2ce5ddf7cf00bb1
ridha-arlian <ridhaarlian@proton.me>
2026-02-25 00:15:35 +0700

    deps: update dependencies

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,7 +1,15 @@
-import { PrismaClient } from '@/generated/prisma'
+// import { PrismaClient } from '@/generated/prisma'
  
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
  
-export const prisma = globalForPrisma.prisma || new PrismaClient({ log: ['query'] })
+// export const prisma = globalForPrisma.prisma || new PrismaClient({ log: ['query'] })
  
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client";
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+export { prisma };
