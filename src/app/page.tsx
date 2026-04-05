25a638ac24194e6a7d99b11edd5cb1304421486a
Ridha Arlian Alhaqie <ridhaarlian@proton.me>
2026-04-05 14:31:15 +0700

    fix(landing): uncomment authentication and redirect logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-// import { auth } from "../../auth"
-import { Box } from "@chakra-ui/react"
-// import { redirect } from "next/navigation"
+import { auth } from "../../auth"
+import { Box } from "@chakra-ui/react"import { redirect } from "next/navigation"
 import { LandingPage } from "@/app/components/landing/landingPage"
 
 export default function Home() {
-  // const session = await auth()
+  const session = await auth()
   
-  // if (session?.user) {
-  //   redirect('/story')
-  // }
+  if (session?.user) {
+    redirect('/story')
+  }
   return (
     <>
       <Box bg="bg.canvas">
